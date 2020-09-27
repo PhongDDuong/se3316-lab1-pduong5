cfce46e6e2f5d1475ecfbb31250e4f56fc73c293
Phong Duong <pduong5@uwo.ca>
2020-09-27 11:30:09 -0400

    improved formating of results and fixed bugs with search

--- pokedex.js
+++ pokedex.js
@@ -1,17 +1,17 @@
-let pokemon = [["1","bulbasaur","grass,poison"],["2","ivysaur","grass,poison"],["3","venusaur","grass,poison"],["4","charmander","fire"],["5","charmeleon","fire"],
-["6","charizard","fire,flying"],["7","squirtle","water"],["8","wartortle","water"],["9","blastoise","water"],["10","caterpie","bug"],["11","metapod","bug"],
-["12","butterfree","bug,flying"],["13","weedle","bug,poison"],["14","kakuna","bug,poison"],["15","beedrill","bug,poison"],["16","pidgey","normal,flying"],
-["17","pidgeotto","normal,flying"],["18","pidgeot","normal,flying"],["19","rattata","normal"],["20","raticate","normal"]]
+let pokemon = [["1","Bulbasaur","Grass, Poison"],["2","Ivysaur","Grass, Poison"],["3","Venusaur","Grass, Poison"],["4","Charmander","Fire"],["5","Charmeleon","Fire"],
+["6","Charizard","Fire, Flying"],["7","Squirtle","Water"],["8","Wartortle","Water"],["9","Blastoise","Water"],["10","Caterpie","Bug"],["11","Metapod","Bug"],
+["12","Butterfree","Bug, Flying"],["13","Weedle","Bug, Poison"],["14","Kakuna","Bug, Poison"],["15","Beedrill","Bug, Poison"],["16","Pidgey","Normal, Flying"],
+["17","Pidgeotto","Normal, Flying"],["18","Pidgeot","Normal, Flying"],["19","Rattata","Normal"],["20","Raticate","Normal"]]
 
 function numSearch(){
     var uInput = document.getElementById("numInput").value;
     let result = "";
     let resultCount = 0;
+
     if(uInput!="" && parseInt(uInput) >= 1 && parseInt(uInput) <= 20){
-        var arrayLength = pokemon.length;
-        for (var i = 0; i < arrayLength && resultCount<5; i++) {
+        for (var i = 0; i < pokemon.length && resultCount<5; i++) {
             if(pokemon[i][0].includes(uInput)){
-                result+=pokemon[i][1]+"\n";
+                result+="#"+pokemon[i][0]+" "+pokemon[i][1]+" | Type: "+pokemon[i][2]+"\n";
                 resultCount++;
             }
         }
@@ -28,15 +28,14 @@ function numSearch(){
 }
 
 function stringSearch(){
-    var uInput = document.getElementById("stringInput").value;
+    var uInput = document.getElementById("stringInput").value.toLowerCase();
     let result = "";
     let resultCount = 0;
 
-    if(uInput!="" && uInput.length <= 20){
-        var arrayLength = pokemon.length;
-        for (var i = 0; i < arrayLength && resultCount<5; i++) {
-            if(pokemon[i][1].includes(uInput)||(pokemon[i][2].includes(uInput)&&uInput.length>2)){
-                result+=pokemon[i][1]+"\n";
+    if(uInput!="" && uInput.length <= 20 && !/[^a-zA-Z]/.test(uInput)){
+        for (var i = 0; i < pokemon.length && resultCount<5; i++) {
+            if(pokemon[i][1].toLowerCase().includes(uInput)||(pokemon[i][2].toLowerCase().includes(uInput)&&uInput.length>2)){
+                result+="#"+pokemon[i][0]+" "+pokemon[i][1]+" | Type: "+pokemon[i][2]+"\n";
                 resultCount++;
             }
         }
