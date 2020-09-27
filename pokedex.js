a3ce5c3a5b36dad23aaa7d8df108827b4c5514c9
Phong Duong <pduong5@uwo.ca>
2020-09-27 10:23:22 -0400

    created pokemon array list

---
+++ pokedex.js
@@ -0,0 +1,4 @@
+let pokemon = [["1","bulbasaur","grass,poison"],["2","ivysaur","grass,poison"],["3","venusaur","grass,poison"],["4","charmander","fire"],["5","charmeleon","fire"],
+["6","charizard","fire,flying"],["7","squirtle","water"],["8","wartortle","water"],["9","blastoise","water"],["10","caterpie","bug"],["11","metapod","bug"],
+["12","butterfree","bug,flying"],["13","weedle","bug,poison"],["14","kakuna","bug,poison"],["15","beedrill","bug,poison"],["16","pidgey","normal,flying"],
+["17","pidgeotto","normal,flying"],["18","pidgeot","normal,flying"],["19","rattata","normal"],["20","raticate","normal"]]
