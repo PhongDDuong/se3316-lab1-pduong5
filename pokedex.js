1061d7e55882befcc6768c3de0767fed4b25e8db
Phong Duong <pduong5@uwo.ca>
2020-10-04 02:06:24 -0400

    reduced redundency of create pokemon

--- pokedex.js
+++ pokedex.js
@@ -37,49 +37,36 @@ function stringSearch(){
     if(uInput!="" && uInput.length <= 20 && !/[^a-zA-Z]/.test(uInput)){//runs as long as the input isn't blank, input is less than 20 characters, and only contains letters
         for (var i = 0; i < pokemon.length; i++) {//iterates through the pokemon list until every pokemon is checked
             if(pokemon[i][1].toLowerCase().includes(uInput)||(pokemon[i][2].toLowerCase().includes(uInput)&&uInput.length>2)){//if string is found in name or typing
-                var node = document.createElement("li");
-                node.id = pokemon[i][1];
-                node.className ="card";
-
-                var pokeName = document.createElement("p");
-                pokeName.textContent = "#"+pokemon[i][0]+" "+pokemon[i][1];
-                pokeName.className ="card-title";
-                var image = document.createElement("img"); 
-                image.src="pokemon/"+(i+1)+".png"
-
-                var desc = document.createElement("p");
-                desc.textContent = "Type: "+pokemon[i][2];
-                desc.className ="card-subtitle";
-
-                document.getElementById("search").appendChild(node);
-                document.getElementById(pokemon[i][1]).appendChild(pokeName);
-                document.getElementById(pokemon[i][1]).appendChild(image);
-                document.getElementById(pokemon[i][1]).appendChild(desc);
+                createPokemon(i,"search");
             }
         }
     }
 }
 
+function createPokemon(x,location){
+    var node = document.createElement("li");
+    node.id = pokemon[x][1];
+    node.className ="card";
+
+    var pokeName = document.createElement("p");
+    pokeName.textContent = "#"+pokemon[x][0]+" "+pokemon[x][1];
+    pokeName.className ="card-title";
+
+    var image = document.createElement("img"); 
+    image.src="pokemon/"+(x+1)+".png"
+
+    var desc = document.createElement("p");
+    desc.textContent = "Type: "+pokemon[x][2];
+    desc.className ="card-subtitle";
+
+    document.getElementById(location).appendChild(node);
+    document.getElementById(pokemon[x][1]).appendChild(pokeName);
+    document.getElementById(pokemon[x][1]).appendChild(image);
+    document.getElementById(pokemon[x][1]).appendChild(desc);
+}
+
 function createAllPokemon(){
-    for (var i = 0; i < pokemon.length; i++) {
-        var node = document.createElement("li");
-        node.id = pokemon[i][1];
-        node.className ="card";
-
-        var pokeName = document.createElement("p");
-        pokeName.textContent = "#"+pokemon[i][0]+" "+pokemon[i][1];
-        pokeName.className ="card-title";
-
-        var image = document.createElement("img"); 
-        image.src="pokemon/"+(i+1)+".png"
-
-        var desc = document.createElement("p");
-        desc.textContent = "Type: "+pokemon[i][2];
-        desc.className ="card-subtitle";
-
-        document.getElementById("pokedex").appendChild(node);
-        document.getElementById(pokemon[i][1]).appendChild(pokeName);
-        document.getElementById(pokemon[i][1]).appendChild(image);
-        document.getElementById(pokemon[i][1]).appendChild(desc);
+    for(var i =0; i < 20 ; i++){
+        createPokemon(i,"pokedex")
     }
 }
